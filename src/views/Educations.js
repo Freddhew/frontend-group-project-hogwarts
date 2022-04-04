4bd87198c563fd1e8d109319e82729a6698b91b4
Mohammad Arefi <m-arefi@live.com>
2022-04-04 14:43:34 +0200

    Educatuin function draft created

--- src/views/Educations.js
+++ src/views/Educations.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import './css/EducationsStyles.css'
-import { get, post, put, erase } from ''
+import { get, post, put, erase } from '../utility/fetchUtility.js'
+import {useState, useEffect } from 'react'
 
-const Educations = () => {
+    function Educations(props) {
+        const [id, setId] = useState(Date.now());
+        const [newId, setNewId] = useState(Date.now());
+      
+        const [leaders, setLeaders] = useState([]);
+        const [courses, setCourses] = useState([]);
+        const [eventLists, seteventLists] = useState([]);
+      
+        const [chooseCourse, setChooseCourse] = useState("");
+        const [chooseEvent, setChooseEvent] = useState("");
+        const [chooseLeader, setChooseLeader] = useState("");
+        const [chooseDescription, setChooseDescription] = useState("");
+      
+        useEffect(() => {
+          get("/Education").then((response) => seteventLists(response.data));
+          get("/Courses").then((response) => setCourses(response.data));
+          get("/Staff").then((response) => setLeaders(response.data));
+        }, []);
+
     return (
         <div className='educations'>
             <div className='card-container'>
