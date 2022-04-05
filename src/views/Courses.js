5e63f2b2758edbd28f519bf1aa68c067ec83405f
Freddhew <fredrik_wisborg@hotmail.com>
2022-04-05 13:45:40 +0200

    Css Update

--- src/views/Courses.js
+++ src/views/Courses.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { get, post, put, remove } from "../utility/fetchUtility";
+import { slideUp, slideDown } from "../utility/slideFunctions";
 import { useState, useEffect } from "react";
 import "./css/courses.css";
 
@@ -18,11 +19,41 @@ function Courses() {
     get("/Staff").then((response) => setTeacher(response.data));
     console.log(`Course: ${course}`);
     scrollToSection();
+    hideSettings(0);
+    setTimeout(() => { hideSettings(0); }, 250);
   }, []);
+
+  let isDisplayingSettings = false;
 
   function scrollToSection(){
     const viewPort = document.getElementById("course-main");
     viewPort.scrollIntoView({behavior: 'smooth'});
+  }
+  
+  function hideSettings(time){
+    let courseSettings = document.getElementsByClassName("course-settings")
+    for (var i = 0; i < courseSettings.length; i++) {
+      console.log(courseSettings.item(i));
+      slideUp(courseSettings.item(i), time);
+    }
+  }
+
+  function showSettings(time){
+    let courseSettings = document.getElementsByClassName("course-settings")
+    for (var i = 0; i < courseSettings.length; i++) {
+      console.log(courseSettings.item(i));
+      slideDown(courseSettings.item(i), time);
+    }
+  }
+
+  function displaySettings(){
+    if (isDisplayingSettings === false){
+      showSettings(200);
+      isDisplayingSettings = true;
+    } else {
+      hideSettings(200);
+      isDisplayingSettings = false;
+    }
   }
 
 
@@ -54,10 +85,10 @@ function Courses() {
 
                         <p>
                         <br/>
-                        <button className="course-remove-by-id-btn" 
+                        <button className="course-remove-by-id-btn course-settings" 
                                 onClick={() => {
-                          remove(`/Courses/${courses.courseId}`);
-                          get("/Courses").then((response) => setCourse(response.data));
+                            remove(`/Courses/${courses.courseId}`);
+                            get("/Courses").then((response) => setCourse(response.data));
                         }}>Remove Course with ID: {courses.courseId}</button></p>
                       </li>
                     </div>
@@ -66,24 +97,35 @@ function Courses() {
               </ul>
       </div>
 
+      <div className="display-settings-btn-div">
+        <button className="display-settings-btn" onClick={() => {
+          displaySettings();
+        }}>
+        Edit Courses
+        </button>
+      </div>
+
       {/* Course Settings */}
       {/* Course Settings */}
       {/* Course Settings */}
       {/* Course Settings */}
-      <div className="course-object-settings">
+      <div className="course-object-settings course-settings">
         <ul>
+          <li className="course-settings-header">  
+            Course Settings
+          </li>
           <li>
-          <input    className='' 
+            <input    className='course-input' 
                       value={courseName} 
                       placeholder="Course Name" 
                       onChange={(e) => setCourseName(e.target.value)}></input>
           </li>
           <li>
-            <select className='' 
+            <select className='course-input' 
                       value={chooseTeacher} 
                       onChange={(event) => setChooseTeacher(event.target.value)}>
               <option value="" 
-                      selected hidden>Välj</option>
+                      selected hidden>Select Teacher</option>
               {teacher.map((teachers) => {
                 if (teachers.profession === "teacher") {
                   return (
@@ -96,10 +138,10 @@ function Courses() {
             </select>
           </li>
           <li>
-            <input value={courseDescription} placeholder="Course Description" onChange={(e) => setCourseDescription(e.target.value)}></input>
+            <input className="course-input course-description-height" value={courseDescription} placeholder="Course Description" onChange={(e) => setCourseDescription(e.target.value)}></input>
           </li>
           <li>
-            <input value={courseLength} placeholder="Course Duration" onChange={(e) => setCourseLength(e.target.value)}></input>
+            <input className="course-input" value={courseLength} placeholder="Course Duration (Weeks)" onChange={(e) => setCourseLength(e.target.value)}></input>
           </li>
           <li>
             <button className="course-add-new-course-btn"
@@ -114,7 +156,8 @@ function Courses() {
                       setCounter(Date.now());
                       get("/Courses").then((response) => setCourse(response.data));
               }}>Create New Course</button>
-            <button onClick={() => {
+            <button className="course-add-new-course-btn"
+                    onClick={() => {
                 put(`/Courses/${id}`, {
                     id: id,
                     courseName: courseName,
