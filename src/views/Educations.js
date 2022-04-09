13e5bcaf84cb2c93d242d9709e242bdb0dd27809
Mohammad Arefi <m-arefi@live.com>
2022-04-09 19:14:52 +0200

    Edu-prettify

--- src/views/Educations.js
+++ src/views/Educations.js
@@ -52,68 +52,68 @@ return (
                     </ul>
                 </div>
             </div>
-        </div>
+    </div>
 
-        <div className='card-container'>
-            <div className='card selectors'>
-                <h3>Enter Education Information</h3>
-                <p>- Education -</p>
-                <select className='selection' value={chooseTitle} onChange={(event) => setChooseTitle(event.target.value)}>
-                    <option value="" disabled selected>Select your education</option>
-                    {eventLists.map((eventList) => {
-                    return (
-                    <option className="option">
-                        {`${eventList.title}`}
-                    </option>
-                    );
-                    })}
-                </select>
-
-                <p>- Education Leader -</p>
-                <select className='selection' value={chooseLeader} onChange={(event) => setChooseLeader(event.target.value)}>
-                    <option value="" disabled selected>Choose your professor</option>
-                    {leaders.map((leader) => {
-                    if (leader.profession === "Leader") {
-                    return (
-                    <option className="selection">
-                        {`${leader.firstName} ${leader.lastName} `}
-                    </option>
-                    );
-                    }
-                    })} 
-                </select>
-
-                <p>- Prerequisite Courses -</p>
-                <select className='selection' value={chooseCourse} onChange={(event) => setChooseCourse(event.target.value)}>
-                    <option value="" disabled selected>Choose a course</option>
-                    {courses.map((course) => {
-                    return (
-                    <option className="option">
-                    {` ${course.courseName}`}
-                    </option>
-                    );
-                    })} 
-                </select>
-
-                <p>- Education Descriptions -</p>
-                <textarea value={chooseDescription} onChange={(event) => setChooseDescription(event.target.value)} placeholder='Share your thoughts with us...'></textarea>
-                <button className='btn' onClick={() => {
-                post("/Education", {
-                Leader: chooseLeader,
-                Title: chooseTitle,
-                Prerequisite: chooseCourse,
-                Description: chooseDescription,
-                })
-                get("/Education").then((response) =>
-                seteventLists(response.data)
+    <div className='card-container'>
+        <div className='card selectors'>
+            <h3>Enter Education Information</h3>
+            <p>- Education -</p>
+            <select className='selection' value={chooseTitle} onChange={(event) => setChooseTitle(event.target.value)}>
+                <option value="" disabled selected>Select your education</option>
+                {eventLists.map((eventList) => {
+                return (
+                <option className="option">
+                    {`${eventList.title}`}
+                </option>
                 );
-                get("/Courses").then((response) => setCourses(response.data));
-                get("/Staff").then((response) => setLeaders(response.data));
-                }}>Submit</button>
-            </div>
+                })}
+            </select>
+
+            <p>- Education Leader -</p>
+            <select className='selection' value={chooseLeader} onChange={(event) => setChooseLeader(event.target.value)}>
+                <option value="" disabled selected>Choose your professor</option>
+                {leaders.map((leader) => {
+                if (leader.profession === "Leader") {
+                return (
+                <option className="selection">
+                    {`${leader.firstName} ${leader.lastName} `}
+                </option>
+                );
+                }
+                })} 
+            </select>
+
+            <p>- Prerequisite Courses -</p>
+            <select className='selection' value={chooseCourse} onChange={(event) => setChooseCourse(event.target.value)}>
+                <option value="" disabled selected>Choose a course</option>
+                {courses.map((course) => {
+                return (
+                <option className="option">
+                {` ${course.courseName}`}
+                </option>
+                );
+                })} 
+            </select>
+
+            <p>- Education Descriptions -</p>
+            <textarea value={chooseDescription} onChange={(event) => setChooseDescription(event.target.value)} placeholder='Share your thoughts with us...'></textarea>
+            <button className='btn' onClick={() => {
+            post("/Education", {
+            Leader: chooseLeader,
+            Title: chooseTitle,
+            Prerequisite: chooseCourse,
+            Description: chooseDescription,
+            })
+            get("/Education").then((response) =>
+            seteventLists(response.data)
+            );
+            get("/Courses").then((response) => setCourses(response.data));
+            get("/Staff").then((response) => setLeaders(response.data));
+            }}>Submit</button>
         </div>
     </div>
-    )
-}
+    </div>
+)}
+
 
 export default Educations
