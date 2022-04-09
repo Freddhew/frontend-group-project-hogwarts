3a806d21ec3aedb45e5c290a2abd23225f84f0b9
Mohammad Arefi <m-arefi@live.com>
2022-04-09 21:42:05 +0200

    Education removed from Home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
+import { Link } from 'react-router-dom'
 import './HeaderStyles.css'
-
 import wandVideo from './Wand.mp4'
 
 const Header = () => {
@@ -11,11 +11,12 @@ const Header = () => {
             <source src={wandVideo} type='video/mp4' />
         </video>
     <div className='content'>
-        <h1>Hogwarts</h1>
-        <p>Take your first step towards a magical future!</p>
+        <h1>Hogwarts</h1><br></br><br></br><br></br>
+        <p>Take your first step towards a magical future!</p><br></br><br></br><br></br>
     <div>
-        <button to='/educations' className='btn'>Alohamora</button>
-        <button to='/contact' className='btn btn-light'>Learn More</button>
+        <botton to='/educations' className='btn'>Alohamora</botton>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <botton to='/contact' className='btn btn-light'>Learn More</botton>
     </div>
     </div>
     </div>
